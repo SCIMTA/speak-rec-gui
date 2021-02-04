0412278807ec027c86f29d1e1d4396a7f5965a19
hoang <neckgamervn@gmail.com>
2021-02-04 18:33:53 +0700

    fix ui

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -67,10 +67,9 @@ namespace SpeakRec
             this.btnOpenFile.Enabled = false;
             this.btnOpenFile.FlatAppearance.BorderSize = 0;
             this.btnOpenFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOpenFile.Location = new System.Drawing.Point(49, 11);
-            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpenFile.Location = new System.Drawing.Point(37, 9);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(33, 31);
+            this.btnOpenFile.Size = new System.Drawing.Size(25, 25);
             this.btnOpenFile.TabIndex = 1;
             this.btnOpenFile.UseVisualStyleBackColor = false;
             this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
@@ -83,10 +82,9 @@ namespace SpeakRec
             this.btnShowSub.Enabled = false;
             this.btnShowSub.FlatAppearance.BorderSize = 0;
             this.btnShowSub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnShowSub.Location = new System.Drawing.Point(131, 11);
-            this.btnShowSub.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShowSub.Location = new System.Drawing.Point(98, 9);
             this.btnShowSub.Name = "btnShowSub";
-            this.btnShowSub.Size = new System.Drawing.Size(33, 31);
+            this.btnShowSub.Size = new System.Drawing.Size(25, 25);
             this.btnShowSub.TabIndex = 10;
             this.btnShowSub.UseVisualStyleBackColor = false;
             this.btnShowSub.Click += new System.EventHandler(this.btnShowSub_Click);
@@ -95,10 +93,9 @@ namespace SpeakRec
             // 
             this.listPerson.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.listPerson.HideSelection = false;
-            this.listPerson.Location = new System.Drawing.Point(1191, 32);
-            this.listPerson.Margin = new System.Windows.Forms.Padding(4);
+            this.listPerson.Location = new System.Drawing.Point(893, 26);
             this.listPerson.Name = "listPerson";
-            this.listPerson.Size = new System.Drawing.Size(477, 200);
+            this.listPerson.Size = new System.Drawing.Size(364, 163);
             this.listPerson.TabIndex = 12;
             this.listPerson.UseCompatibleStateImageBehavior = false;
             // 
@@ -109,10 +106,9 @@ namespace SpeakRec
             this.btnShowListPerson.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnShowListPerson.FlatAppearance.BorderSize = 0;
             this.btnShowListPerson.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnShowListPerson.Location = new System.Drawing.Point(8, 11);
-            this.btnShowListPerson.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShowListPerson.Location = new System.Drawing.Point(6, 9);
             this.btnShowListPerson.Name = "btnShowListPerson";
-            this.btnShowListPerson.Size = new System.Drawing.Size(33, 31);
+            this.btnShowListPerson.Size = new System.Drawing.Size(25, 25);
             this.btnShowListPerson.TabIndex = 14;
             this.btnShowListPerson.UseVisualStyleBackColor = false;
             this.btnShowListPerson.Click += new System.EventHandler(this.btnShowListPerson_Click);
@@ -121,10 +117,9 @@ namespace SpeakRec
             // 
             this.ListSub.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.ListSub.HideSelection = false;
-            this.ListSub.Location = new System.Drawing.Point(0, 240);
-            this.ListSub.Margin = new System.Windows.Forms.Padding(4);
+            this.ListSub.Location = new System.Drawing.Point(9, 195);
             this.ListSub.Name = "ListSub";
-            this.ListSub.Size = new System.Drawing.Size(1684, 591);
+            this.ListSub.Size = new System.Drawing.Size(1248, 407);
             this.ListSub.TabIndex = 15;
             this.ListSub.UseCompatibleStateImageBehavior = false;
             this.ListSub.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ListSub_ItemSelectionChanged);
@@ -137,10 +132,9 @@ namespace SpeakRec
             this.btnRecord.Enabled = false;
             this.btnRecord.FlatAppearance.BorderSize = 0;
             this.btnRecord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRecord.Location = new System.Drawing.Point(90, 11);
-            this.btnRecord.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRecord.Location = new System.Drawing.Point(68, 9);
             this.btnRecord.Name = "btnRecord";
-            this.btnRecord.Size = new System.Drawing.Size(33, 31);
+            this.btnRecord.Size = new System.Drawing.Size(25, 25);
             this.btnRecord.TabIndex = 16;
             this.btnRecord.Tag = "r";
             this.btnRecord.UseVisualStyleBackColor = false;
@@ -154,10 +148,9 @@ namespace SpeakRec
             this.btnExportText.Enabled = false;
             this.btnExportText.FlatAppearance.BorderSize = 0;
             this.btnExportText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExportText.Location = new System.Drawing.Point(172, 11);
-            this.btnExportText.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExportText.Location = new System.Drawing.Point(129, 9);
             this.btnExportText.Name = "btnExportText";
-            this.btnExportText.Size = new System.Drawing.Size(33, 31);
+            this.btnExportText.Size = new System.Drawing.Size(25, 25);
             this.btnExportText.TabIndex = 17;
             this.btnExportText.UseVisualStyleBackColor = false;
             this.btnExportText.Click += new System.EventHandler(this.btnExportText_Click);
@@ -166,11 +159,10 @@ namespace SpeakRec
             // 
             this.tbSub.Enabled = false;
             this.tbSub.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.tbSub.Location = new System.Drawing.Point(12, 122);
-            this.tbSub.Margin = new System.Windows.Forms.Padding(4);
+            this.tbSub.Location = new System.Drawing.Point(9, 99);
             this.tbSub.Multiline = true;
             this.tbSub.Name = "tbSub";
-            this.tbSub.Size = new System.Drawing.Size(1165, 110);
+            this.tbSub.Size = new System.Drawing.Size(875, 90);
             this.tbSub.TabIndex = 18;
             this.tbSub.TextChanged += new System.EventHandler(this.tbSub_TextChanged);
             // 
@@ -178,10 +170,9 @@ namespace SpeakRec
             // 
             this.tbName.Enabled = false;
             this.tbName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.tbName.Location = new System.Drawing.Point(12, 82);
-            this.tbName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbName.Location = new System.Drawing.Point(9, 67);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(725, 30);
+            this.tbName.Size = new System.Drawing.Size(545, 26);
             this.tbName.TabIndex = 19;
             this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
             // 
@@ -194,8 +185,9 @@ namespace SpeakRec
             this.textToolStripMenuItem});
             this.menuMain.Location = new System.Drawing.Point(0, 0);
             this.menuMain.Name = "menuMain";
+            this.menuMain.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
             this.menuMain.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.menuMain.Size = new System.Drawing.Size(1685, 28);
+            this.menuMain.Size = new System.Drawing.Size(1264, 24);
             this.menuMain.TabIndex = 20;
             this.menuMain.Text = "menuStrip1";
             // 
@@ -207,34 +199,34 @@ namespace SpeakRec
             this.managerToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // openFileToolStripMenuItem
             // 
             this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
-            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.openFileToolStripMenuItem.Text = "Mở file";
             this.openFileToolStripMenuItem.Click += new System.EventHandler(this.btnOpenFile_Click);
             // 
             // recordToolStripMenuItem
             // 
             this.recordToolStripMenuItem.Name = "recordToolStripMenuItem";
-            this.recordToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.recordToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.recordToolStripMenuItem.Text = "Ghi âm";
             this.recordToolStripMenuItem.Click += new System.EventHandler(this.btnRecord_Click);
             // 
             // managerToolStripMenuItem
             // 
             this.managerToolStripMenuItem.Name = "managerToolStripMenuItem";
-            this.managerToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.managerToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.managerToolStripMenuItem.Text = "Quản trị";
             this.managerToolStripMenuItem.Click += new System.EventHandler(this.managerToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
             this.exitToolStripMenuItem.Text = "Thoát";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -243,13 +235,13 @@ namespace SpeakRec
             this.personJoinToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.listPersonJoinToolStripMenuItem});
             this.personJoinToolStripMenuItem.Name = "personJoinToolStripMenuItem";
-            this.personJoinToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.personJoinToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
             this.personJoinToolStripMenuItem.Text = "Người tham gia";
             // 
             // listPersonJoinToolStripMenuItem
             // 
             this.listPersonJoinToolStripMenuItem.Name = "listPersonJoinToolStripMenuItem";
-            this.listPersonJoinToolStripMenuItem.Size = new System.Drawing.Size(266, 26);
+            this.listPersonJoinToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
             this.listPersonJoinToolStripMenuItem.Text = "Danh sách người tham gia";
             this.listPersonJoinToolStripMenuItem.Click += new System.EventHandler(this.btnShowListPerson_Click);
             // 
@@ -259,20 +251,20 @@ namespace SpeakRec
             this.showSubToolStripMenuItem,
             this.exportSubToolStripMenuItem});
             this.textToolStripMenuItem.Name = "textToolStripMenuItem";
-            this.textToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.textToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.textToolStripMenuItem.Text = "Văn bản";
             // 
             // showSubToolStripMenuItem
             // 
             this.showSubToolStripMenuItem.Name = "showSubToolStripMenuItem";
-            this.showSubToolStripMenuItem.Size = new System.Drawing.Size(204, 26);
+            this.showSubToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.showSubToolStripMenuItem.Text = "Xem kèm phụ đề";
             this.showSubToolStripMenuItem.Click += new System.EventHandler(this.btnShowSub_Click);
             // 
             // exportSubToolStripMenuItem
             // 
             this.exportSubToolStripMenuItem.Name = "exportSubToolStripMenuItem";
-            this.exportSubToolStripMenuItem.Size = new System.Drawing.Size(204, 26);
+            this.exportSubToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.exportSubToolStripMenuItem.Text = "Xuất phụ đề";
             this.exportSubToolStripMenuItem.Click += new System.EventHandler(this.btnExportText_Click);
             // 
@@ -280,20 +272,18 @@ namespace SpeakRec
             // 
             this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBox1.Location = new System.Drawing.Point(744, 82);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(558, 67);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 30);
+            this.textBox1.Size = new System.Drawing.Size(160, 26);
             this.textBox1.TabIndex = 21;
             // 
             // textBox2
             // 
             this.textBox2.Enabled = false;
             this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBox2.Location = new System.Drawing.Point(965, 82);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(724, 67);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(212, 30);
+            this.textBox2.Size = new System.Drawing.Size(160, 26);
             this.textBox2.TabIndex = 22;
             // 
             // groupBox1
@@ -303,19 +293,18 @@ namespace SpeakRec
             this.groupBox1.Controls.Add(this.btnShowListPerson);
             this.groupBox1.Controls.Add(this.btnOpenFile);
             this.groupBox1.Controls.Add(this.btnRecord);
-            this.groupBox1.Location = new System.Drawing.Point(12, 32);
+            this.groupBox1.Location = new System.Drawing.Point(9, 26);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(1167, 47);
+            this.groupBox1.Size = new System.Drawing.Size(875, 38);
             this.groupBox1.TabIndex = 23;
             this.groupBox1.TabStop = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.ClientSize = new System.Drawing.Size(1264, 609);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -327,7 +316,6 @@ namespace SpeakRec
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuMain;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
